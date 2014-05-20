ed083a3308f3b48cefd56f2ae5f5cd52477b7cc0
talaye <h.ataie90@gmail.com>
2014-05-20 21:58:55 +0430

    firstcommit

--- sakhteman1/models.py
+++ sakhteman1/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class test (models.Model):
+    texr = models.CharField(max_length=100)
--- sakhteman1/views.py
+++ sakhteman1/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from sakhteman1.database import sakhtemandb
 
 # Create your views here.
